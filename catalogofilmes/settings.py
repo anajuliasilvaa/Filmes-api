7c78620a805ab3a89fc7edf6f49366a6379f7b11
anajuliasilvaa <anajudasilvaalmeida@gmail.com>
2025-11-30 20:57:06 -0300

    configuracao cors para o front

--- catalogofilmes/settings.py
+++ catalogofilmes/settings.py
@@ -49,11 +49,13 @@ INSTALLED_APPS = [
     'catalogofilmes',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'corsheaders',  # CORS para permitir requisições do frontend
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # CORS deve vir antes do CommonMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -176,7 +178,24 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": True,
 }
 
+# CORS Configuration
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Next.js dev server
+    "http://127.0.0.1:3000",
+]
 
+CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 # ALLOWED_HOSTS = ['*']
